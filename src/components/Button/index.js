a6d4c2f9ee25f2178d551f2e6c2155569654e819
tao-a-man <ducthinhnguyenluong@gmail.com>
2022-05-20 14:38:03 +0700

    prettierrc

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
-import myAlert from "~/myAlert";
+import myAlert from '~/myAlert';
 function Button() {
-    return <button onClick={myAlert}>Click me!</button>
+    return <button onClick={myAlert}>Click me!</button>;
 }
 export default Button;
